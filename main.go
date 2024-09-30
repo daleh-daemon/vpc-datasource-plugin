e9881cb0c73f72a6bf6d2f7d02995b3b1c178568
daleh <dale.hough@dae.mn>
2024-09-30 16:26:23 +0100

    added in metadata

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+    "fmt"
     "github.com/daleh-daemon/packer-vpc-datasoruce-plugin/datasources"
     "github.com/hashicorp/packer-plugin-sdk/plugin"
+    "github.com/hashicorp/packer-plugin-sdk/version"
+    "os"
 )
 
 func main() {
     pps := plugin.NewSet()
+    pps.SetVersion(version.InitializePluginVersion("0.0.1", "dev"))
     pps.RegisterDatasource("aws_vpc", new(datasources.Datasource))
+    err := pps.Run()
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err.Error())
+        os.Exit(1)
+    }
 }
